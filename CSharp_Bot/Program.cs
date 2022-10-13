9b1c07f776be4840b184f8129458f8f5b44ba0cc
Navist <zachstheman2@hotmail.com>
2022-10-13 17:41:33 -0400

    Initial bot construction

--- CSharp_Bot/Program.cs
+++ CSharp_Bot/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using Discord;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+// https://discord.com/api/oauth2/authorize?client_id=1030218384094281818&permissions=8&scope=bot
 
 namespace CSharp_Bot
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static Task Main(string[] args) => new Program().MainAsync();
+        private DiscordSocketClient _client;
+
+        private Task Log(LogMessage msg)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
+
+        public async Task MainAsync()
+        {
+            {
+                _client = new DiscordSocketClient();
+
+                _client.Log += Log;
+
+                var token = File.ReadAllText(@"E:\Desktop Stuff\Bot Files\CSharp_Bot\data\token.txt");
+
+                await _client.LoginAsync(TokenType.Bot, token);
+                await _client.StartAsync();
+
+                // Block this task until the program is closed.
+                await Task.Delay(-1);
+            }
+        }
+
     }
+
 }
+
